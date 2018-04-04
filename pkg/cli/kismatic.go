1e961cd5e50bac52e2053a2dd81e5db3ef4f461a
Emmett Hitz <EHitz@apprenda.com>
2018-04-04 10:20:25 -0400

    Remove install as a command. Surface subcommands at the root of the command tree.

--- pkg/cli/kismatic.go
+++ pkg/cli/kismatic.go
@@ -6,8 +6,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type installOpts struct {
+}
+
 // NewKismaticCommand creates the kismatic command
 func NewKismaticCommand(version string, buildDate string, in io.Reader, out, stderr io.Writer) (*cobra.Command, error) {
+	opts := &installOpts{}
 	cmd := &cobra.Command{
 		Use:   "kismatic",
 		Short: "kismatic is the main tool for managing your Kubernetes cluster",
@@ -22,7 +26,6 @@ more documentation is available at https://github.com/apprenda/kismatic`,
 
 	cmd.AddCommand(NewCmdImport(out))
 	cmd.AddCommand(NewCmdVersion(buildDate, out))
-	cmd.AddCommand(NewCmdInstall(in, out))
 	cmd.AddCommand(NewCmdVolume(in, out))
 	cmd.AddCommand(NewCmdIP(out))
 	cmd.AddCommand(NewCmdDashboard(in, out))
@@ -33,6 +36,13 @@ more documentation is available at https://github.com/apprenda/kismatic`,
 	cmd.AddCommand(NewCmdCertificates(out))
 	cmd.AddCommand(NewCmdSeedRegistry(out, stderr))
 	cmd.AddCommand(NewCmdServer(out))
+	cmd.AddCommand(NewCmdPlan(in, out, opts))
+	cmd.AddCommand(NewCmdValidate(out, opts))
+	cmd.AddCommand(NewCmdApply(out, opts))
+	cmd.AddCommand(NewCmdAddNode(out, opts))
+	cmd.AddCommand(NewCmdStep(out, opts))
+	cmd.AddCommand(NewCmdProvision(in, out, opts))
+	cmd.AddCommand(NewCmdDestroy(in, out, opts))
 
 	return cmd, nil
 }
